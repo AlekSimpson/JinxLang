b48d41f56fbed0c13b847aac605d0870c73a7428
AlekSimpson <alek@asu.me>
2022-01-11 11:42:57 -0700

    scripts work from command line now

--- aqua.py
+++ aqua.py
@@ -1,12 +1,37 @@
 from run import run
+import sys
 
-while True:
-    textInput = input("aqua> ")
-    if textInput == "": continue
-    if str(textInput) == "stop":
-        break 
+if len(sys.argv) == 0:
+    while True:
+        textInput = input("aqua> ")
+        if textInput == "": continue
+        if str(textInput) == "stop":
+            break 
 
-    result, error = run(textInput, "repl")
+        result, error = run(textInput, "repl")
+
+        if error != None: 
+            print(error.as_string())
+        elif result != None:
+            if len(result.elements) == 1 and len(result.elements) != 0:
+                if result.elements[0] != None:
+                    print(result.elements[0].print_self())
+            else:
+                for ele in result.elements:
+                    if ele != None: 
+                        print(ele.print_self())
+else:
+    filename = sys.argv[1]
+    script = None 
+    try:
+        with open(filename, "r") as f:
+            script = f.read()
+    except Exception as e:
+        err = RuntimeError("Failed to execute file" + str(e), Context(), Position())
+        res.failure(err)
+        print(err.as_string())
+    
+    result, error = run(script, filename)
 
     if error != None: 
         print(error.as_string())
@@ -18,4 +43,6 @@ while True:
             for ele in result.elements:
                 if ele != None: 
                     print(ele.print_self())
+   
+
 
